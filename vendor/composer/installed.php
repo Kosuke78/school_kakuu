7ca65ebc6ee8bb3cf8e9e821f956b7d50596cf40
Kosuke <bkpc9070@yahoo.ne.jp>
2022-01-20 16:17:28 +0900

    a

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '22f29670f04137142091f3343014e0e2262af7a2',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '22f29670f04137142091f3343014e0e2262af7a2',
+    ),
+  ),
+);
